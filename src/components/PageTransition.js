766fceed292db420b77c8fc4f20760a35be21bd0
Alan Rocha <alan.rocha@dotlib.com.br>
2018-09-23 19:58:03 -0300

    Remoção de console.log desnecessário

--- src/components/PageTransition.js
+++ src/components/PageTransition.js
@@ -11,8 +11,6 @@ const PageTransition = props => {
   const location = props.location;
   const pathname = location.pathname;
 
-  console.log(pathname);
-
   return (
     <TransitionGroup>
       <CSSTransition key={pathname} classNames="page" timeout={timeout}>
